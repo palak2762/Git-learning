66d87548ac43b5bf1fddae1ee4532b9679359a8f
Palak Parmar <palakparmar246@gmail.com>
2026-03-23 23:20:45 +0530

    upi updated after commit

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ console.log(login);
 
 const footer="footer added in our website";
 console.log(footer);
+const payment="integrated the payment gateway";
+console.log(payment);
+
+const upi= "integrated the UPI";
+console.log(upi);
